6020a924e9135c65839ea775e0199d3b0cdf2f16
ilya <pasterzoom@gmail.com>
2023-05-05 23:26:04 +0300

    feat: end 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const {readdir, mkdir, rm, copyFile} = require('fs/promises');
+const path = require('path');
 
+(async () => {
+  const filesPath = path.join(__dirname, 'files');
+  const filedCopyPath = path.join(__dirname, 'files-copy');
+
+  await rm(filedCopyPath, {recursive: true, force: true});
+  await mkdir(filedCopyPath, {recursive: true});
+
+  const folderContents = await readdir(filesPath, {withFileTypes: true});
+
+  folderContents.forEach(file => {
+    copyFile(`${filesPath}/${file.name}`, `${filedCopyPath}/${file.name}`);
+  });
+})();
