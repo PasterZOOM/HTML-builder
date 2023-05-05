c97652fa372dc3f34102c0f039eb7b7c99d05010
ilya <pasterzoom@gmail.com>
2023-05-05 18:02:45 +0300

    feat: end 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const {readdir, stat} = require('fs/promises');
+const path = require('path');
+const filePath = path.join(__dirname, 'secret-folder');
+
+(async () => {
+  const folderContents = await readdir(filePath, {withFileTypes: true});
+
+  await Promise.all(folderContents.map(async file => {
+    const data = await stat(`${filePath}/${file.name}`);
+
+    if (data.isFile()) {
+      const name = file.name.replace(/\.[^.]+$/, '');
+      const extname = path.extname(`${filePath}/${file.name}`).slice(1);
+      const size = data.size * 0.001;
+
+      console.log(`${name} - ${extname} - ${size}kb`);
+    }
+  }));
+})();
